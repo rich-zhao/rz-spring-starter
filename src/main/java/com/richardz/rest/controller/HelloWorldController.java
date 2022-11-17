57fb59671985fa277ca26b58198c04f7a6427f0b
Richard Z <3252909+rich-zhao@users.noreply.github.com>
2022-11-17 15:29:39 -0800

    remove unused variables

--- src/main/java/com/richardz/rest/controller/HelloWorldController.java
+++ src/main/java/com/richardz/rest/controller/HelloWorldController.java
@@ -6,20 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.richardz.rest.model.Person;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * Sample Controller for various REST-based tasks
  */
-@Slf4j //https://projectlombok.org/features/log creates log object
+@Slf4j
 @RestController
 public class HelloWorldController {
-    
-    Gson gson = new Gson();
-    ObjectMapper mapper = new ObjectMapper();
 
     @GetMapping("/hello")
     public String get() {
